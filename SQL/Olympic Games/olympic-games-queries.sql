95fcd0b4542b1de5e602d33c280148c3a24c9683
Fabrizio Cominetti <fabriziocominetti1@gmail.com>
2023-05-29 23:01:37 +0200

    Update olympic-games-queries.sql

--- SQL/Olympic Games/olympic-games-queries.sql
+++ SQL/Olympic Games/olympic-games-queries.sql
@@ -35,3 +35,20 @@ WHERE Games = '1896 Summer';
 SELECT Games, COUNT(DISTINCT NOC) AS number_of_nations, MIN(number_of_nations) AS lowest, MAX(number_of_nations) AS highest
 FROM athlete_events
 GROUP BY Games;
+
+--- Which nation has participated in all of the olympic games?
+--- Identify the sport which was played in all summer olympics.
+--- Which Sports were just played only once in the olympics?
+--- Fetch the total no of sports played in each olympic games.
+--- Fetch details of the oldest athletes to win a gold medal.
+--- Find the Ratio of male and female athletes participated in all olympic games.
+--- Fetch the top 5 athletes who have won the most gold medals.
+--- Fetch the top 5 athletes who have won the most medals (gold/silver/bronze).
+--- Fetch the top 5 most successful countries in olympics. Success is defined by no of medals won.
+--- List down total gold, silver and broze medals won by each country.
+--- List down total gold, silver and broze medals won by each country corresponding to each olympic games.
+--- Identify which country won the most gold, most silver and most bronze medals in each olympic games.
+--- Identify which country won the most gold, most silver, most bronze medals and the most medals in each olympic games.
+--- Which countries have never won gold medal but have won silver/bronze medals?
+--- In which Sport/event, India has won highest medals.
+--- Break down all olympic games where india won medal for Hockey and how many medals in each olympic games.
